f125852cd60d0a670b4d0a097e5dcd9be9d753c5
martinnegro <mnegro91@gmail.com>
2021-03-24 03:22:43 -0300

    Juego terminado. Estilo básico para funcionalidad.

--- script.js
+++ script.js
@@ -5,28 +5,25 @@ let ppt = ['& PIEDRA &', '# PAPEL #', '8< TIJERA 8<'];
 /*Random para elección de computadora.
 Cambia la eleccion de la computadora en cada vuelta */
 let computerSelection = function cS() {
-        let random = Math.floor(Math.random() * ppt.length);
+    let random = Math.floor(Math.random() * ppt.length);
         
-        const computadora = document.querySelector('#computadora')
-        let div = document.querySelector('.eleccionCompu');
+    let div = document.querySelector('.eleccionCompu');
+    div.textContent = ppt[random];
+       
+    return seleccionCompu = ppt[random];
+}
 
-            computadora.removeChild(div) 
-             
-            div = document.createElement('div')
-            div.classList.add('eleccionCompu');
-            div.textContent = ppt[random];
-
-            computadora.appendChild(div)
-        
-        return seleccionCompu = ppt[random];
-
-        }
+function playerSelection() {
+    let div = document.querySelector('.eleccionPlayer');
+    div.textContent = ppt[this.value];
+}
 
 /* Asigna a cada selector de botón por el id
     el evento click y llama la funcion roundPlay*/ 
 const botones = Array.from(document.querySelectorAll('#btn'));
 botones.forEach((btn) => {  
     btn.addEventListener('click', roundPlay);
+    btn.addEventListener('click', playerSelection);
 })
 
 //Una ronda que ejecuta ambas selecciones, las compara
@@ -44,60 +41,52 @@ function roundPlay () {
     } else {
         if (seleccion === seleccionCompu) {
             resultadoRonda = posiblesResultados[2];
-
         } else if (seleccion === ppt[0] && seleccionCompu === ppt[1]) {
             resultadoRonda = posiblesResultados[1];    
-
         } else if (seleccion === ppt[0] && seleccionCompu === ppt[2]) {
             resultadoRonda = posiblesResultados[0];    
-
         } else if (seleccion === ppt[1] && seleccionCompu === ppt[0]) {
             resultadoRonda = posiblesResultados[0];    
-
         } else if (seleccion === ppt[1] && seleccionCompu === ppt[2]) {
             resultadoRonda = posiblesResultados[1];    
-
         } else if (seleccion === ppt[2] && seleccionCompu === ppt[0]) {
             resultadoRonda = posiblesResultados[1];
-
         } else if (seleccion === ppt[2] && seleccionCompu === ppt[1]) {
             resultadoRonda = posiblesResultados[0];
-
         }
     }      
-    console.log(resultadoRonda);
+    contador(resultadoRonda);
 }
 
 // el juego en sí
 // primero describe y luego invoca la ronda
 // el primero que llegue a 5 gana.
-function game() {
-    alert('Piedra, papel o tijera clásico. Escribí tus opciones y competí contra la máquina. El primero que llegue a 5, gana. Suerte.')
-    
-    let puntoPlayer = 0;
-    let puntoCompu = 0;
+let puntoPlayer = 0;
+let puntoCompu = 0;
 
-    do { 
-        let resultadoRonda = roundPlay();
-        if ( resultadoRonda === posiblesResultados[0]) {
-            puntoPlayer = ++puntoPlayer;
-            alert (resultadoRonda + '\n J: ' + puntoPlayer + ' - C: ' + puntoCompu);
-            console.log(resultadoRonda);
-        } else if (resultadoRonda === posiblesResultados[1]) {
+function contador(resultadoRonda) {
+    
+    if ( resultadoRonda === posiblesResultados[0]) {
+        puntoPlayer = ++puntoPlayer;
+    } else if (resultadoRonda === posiblesResultados[1]) {
             puntoCompu = ++puntoCompu;
-            alert (resultadoRonda + '\n / J: ' + puntoPlayer + ' - C: ' + puntoCompu);
-            console.log(resultadoRonda);
-        } else {
-            alert (resultadoRonda + '\n / J: ' + puntoPlayer + ' - C: ' + puntoCompu);
-            console.log(resultadoRonda);
-        }
-    } while (puntoPlayer < 5 && puntoCompu < 5);
+    }
+    
+    let contadorPlayer = document.querySelector('.contadorPlayer');
+    contadorPlayer.textContent = puntoPlayer;
+    let contadorCompu = document.querySelector('.contadorCompu');
+    contadorCompu.textContent = puntoCompu;
 
     if (puntoPlayer === 5) {
-        alert('¡Ganaste! ' + puntoPlayer + ' a ' + puntoCompu + '.\n Pasá a buscar tu don periñon');
-    } else {
-        alert('Perdiste... ' + puntoPlayer + ' a ' + puntoCompu + '.\n Vergüenza.');
-    }    
+        let cabecera = document.querySelector('#cabecera');
+        cabecera.removeChild;
+        cabecera.textContent = 'GANASTE!'
+    } else if (puntoCompu === 5) {
+        let cabecera = document.querySelector('#cabecera');
+        cabecera.removeChild;
+        cabecera.textContent = 'PERDISTE!'
+    }
+
 }
 
 
